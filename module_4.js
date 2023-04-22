b47bc6c2a75411d20b97aeb10fa7e9df2b5f46f8
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-04-23 00:11:30 +0100

    update

--- module_4.js
+++ module_4.js
@@ -495,11 +495,71 @@ console.log(getUserBalance(users));
 // Change code above this line
 */
 //19/48
+/*
 const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
 // Change code below this line
 
-const evenNumbers = numbers;
-const oddNumbers = numbers;
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
+const oddNumbers = numbers.filter((number) => number % 2 !== 0);
 //tests
 console.log(evenNumbers); //[24, 82, 36, 18, 52]
 console.log(oddNumbers); //[17, 61, 47, 73];
+*/
+//20/48
+/*
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
+// Change code below this line
+const allGenres = books.flatMap((book) => book.genres);
+const uniqueGenres = allGenres.filter(
+  (genres, index, books) => books.indexOf(genres) === index
+);
+
+//tests
+console.table(allGenres); //["adventure", "history", "fiction", "mysticism", "horror", "mysticism", "adventure"]
+console.table(uniqueGenres); //["adventure", "history", "fiction", "mysticism", "horror"]
+//Example
+// const uniqueCourses = allCourses.filter((course, index, array) => array.indexOf(course) === index);
+*/
+//21/48
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+
+const MIN_RATING = 8;
+const AUTHOR = "Bernard Cornwell";
+// Change code below this line
+
+const topRatedBooks = books;
+const booksByAuthor = books;
